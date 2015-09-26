ee691e4e5664744a138620747bba7923b4b8a69f
lee allen <leeallen337@gmail.com>
2015-09-26 16:29:52 -0400

    added some requires

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var babel = require('gulp-babel');
+var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
